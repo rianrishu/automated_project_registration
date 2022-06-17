0b7a5856686cb98f997417e42c83bf93435b9ebd
Pratham <pgupta4596@gmail.com>
2022-06-17 23:48:33 +0530

    temp

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,17 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
